16272cfe9c9e2b79ea8fd232d53b3a21dd808092
RabinaSuwal <106600261+RabinaSuwal@users.noreply.github.com>
2024-04-01 19:28:24 +0545

    form created

--- my-project/app/page.js
+++ my-project/app/page.js
@@ -1,9 +1,12 @@
+"use client";
 import Image from "next/image";
+import TodoForm from "./components/TodoForm";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center  p-24">
       <h1>Todo app</h1>
+      <TodoForm/>
     </main>
   );
 }
