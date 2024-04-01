61499021f091f9062384f77448e285f91554a558
RabinaSuwal <106600261+RabinaSuwal@users.noreply.github.com>
2024-04-01 19:56:21 +0545

    add items

--- my-project/app/page.js
+++ my-project/app/page.js
@@ -1,12 +1,13 @@
 "use client";
-import Image from "next/image";
-import TodoForm from "./components/TodoForm";
+//import Image from "next/image";
+import React from "react";
+import AddTodo from "./components/AddTodo";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
       <h1>Todo app</h1>
-      <TodoForm/>
+      <AddTodo/>
     </main>
   );
 }
