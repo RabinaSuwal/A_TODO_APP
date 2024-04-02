0b61491e07be12d9862b49094014cd38b87da820
RabinaSuwal <106600261+RabinaSuwal@users.noreply.github.com>
2024-04-02 18:36:18 +0545

    changes

--- my-project/app/page.js
+++ my-project/app/page.js
@@ -2,7 +2,7 @@
 //import Image from "next/image";
 import React from "react";
 import AddTodo from "./components/AddTodo";
-
+console.log("hello");
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
