73c536fb8b4d08704f00a46fc4d257e8c14d7fa9
RabinaSuwal <106600261+RabinaSuwal@users.noreply.github.com>
2024-04-02 14:43:15 +0545

    setup done

---
+++ my-project/app/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import CheckboxList from './CheckboxList'
+
+export default function TodoList({todos}) {
+  return (
+    <div>
+      
+      <div className="showItems">
+        <ul>
+          {todos.map((todo, index) => (
+            <CheckboxList key={index} todo={todo}/>
+          ))}
+        </ul>
+      </div>
+   
+    </div>
+  )
+}
--- my-project/app/page.js
+++ my-project/app/page.js
@@ -1,12 +1,19 @@
 "use client";
-import Image from "next/image";
-import TodoForm from "./components/TodoForm";
+import React,{useState} from "react";
+import AddTodo from "./components/AddTodo"
+import TodoList from "./components/TodoList"
 
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
       <h1>Todo app</h1>
-      <TodoForm/>
+      <AddTodo addTodo={addTodo}/>
+      <TodoList todos={todos}   />
     </main>
   );
 }
